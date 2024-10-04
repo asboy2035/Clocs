3030b9e61533e3f10b7192e6c961b9bd5f68ba1b
ash <as_supp@icloud.com>
2024-10-04 12:51:47 +0200

    UI improvements and bug fixes!

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Cache name
-const CACHE_NAME = 'clocs-cache';
+// Cache name (consider versioning to help with updates)
+const CACHE_NAME = 'clocs-cache-v1';
 
 // Files to cache
 const FILES_TO_CACHE = [
@@ -18,30 +18,44 @@ self.addEventListener('install', (event) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting(); // Activate the service worker immediately after installation
 });
 
-// Fetch event
+// Activate event - for cleaning up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            // Delete old caches
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // Take control of the page immediately
+});
+
+// Fetch event - network-first approach
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        // If there is a cached response, return it
-        return response;
-      } else {
-        // If there isn't a cached response, try to fetch the resource from the network
-        return fetch(event.request).then((networkResponse) => {
-          // If the network response is successful, cache it
-          if (networkResponse.ok) {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, networkResponse.clone());
-              return networkResponse;
-            });
-          } else {
-            // If the network response isn't successful, return an error
-            return networkResponse;
-          }
+    fetch(event.request)
+      .then((networkResponse) => {
+        // If the network response is successful, update the cache
+        if (networkResponse && networkResponse.ok) {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse; // Return the fresh network response
+          });
+        }
+      })
+      .catch(() => {
+        // If the network fails, serve from cache
+        return caches.match(event.request).then((response) => {
+          return response || fetch(event.request); // Return cached version or attempt another fetch
         });
-      }
-    })
+      })
   );
 });
